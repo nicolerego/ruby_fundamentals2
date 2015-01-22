b77972b58eed34b5a63956e2900dd26494ce3eab
Nicole Rego <nicole.rego@live.com>
2015-01-21 21:09:49 -0500

    Whoops, forgot rice

--- exercise6.rb
+++ exercise6.rb
@@ -2,6 +2,8 @@ grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
 grocery_list.each { |item| puts "* " + item}
 
+grocery_list << "rice"
 
+grocery_list.each { |item| puts "* " + item}
 
 

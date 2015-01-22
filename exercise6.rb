95761ed087f47d24720f39d9917ab2c6a8c7c6fd
Nicole Rego <nicole.rego@live.com>
2015-01-21 21:13:41 -0500

    Checking to see if I need to pick up bananas

--- exercise6.rb
+++ exercise6.rb
@@ -11,3 +11,13 @@ def print_list(list)
 end 
 
 print_list(grocery_list)
+
+puts grocery_list.count
+
+if grocery_list.include?("bananas") 
+	puts "You need to pick up bananas"
+else 
+	puts "You don't need to pick up bananas today"
+end 
+
+puts grocery_list[1]
